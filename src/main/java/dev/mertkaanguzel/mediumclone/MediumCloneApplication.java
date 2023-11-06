f6c676eefd6a948239bea0402157e4f2af8c181e
rtardo <84642031+rtardo@users.noreply.github.com>
2023-11-06 15:39:43 +0300

    Initialize the spring boot app, use the layered structure

---
+++ src/main/java/dev/mertkaanguzel/mediumclone/MediumCloneApplication.java
@@ -0,0 +1,13 @@
+package dev.mertkaanguzel.mediumclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediumCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediumCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/mertkaanguzel/mediumclone/MediumCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mertkaanguzel.mediumclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediumCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
