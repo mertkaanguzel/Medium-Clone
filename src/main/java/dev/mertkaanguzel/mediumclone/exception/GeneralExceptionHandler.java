04f2ce141a3837068097a3149940801e05d3752a
rtardo <84642031+rtardo@users.noreply.github.com>
2023-11-15 18:49:57 +0300

    Implement General Exception Handler to return error responses properly and User Not Found Exception

---
+++ src/main/java/dev/mertkaanguzel/mediumclone/exception/GeneralExceptionHandler.java
@@ -0,0 +1,42 @@
+package dev.mertkaanguzel.mediumclone.exception;
+
+
+import org.jetbrains.annotations.NotNull;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import org.springframework.validation.FieldError;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GeneralExceptionHandler extends ResponseEntityExceptionHandler {
+    @NotNull
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  @NotNull HttpHeaders headers,
+                                                                  @NotNull HttpStatusCode status,
+                                                                  @NotNull WebRequest request
+    ) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach(error -> {
+            String fieldName = ((FieldError) error).getField();
+            String message = error.getDefaultMessage();
+            errors.put(fieldName, message);
+        });
+
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<?> userNotFoundExceptionHandler(UserNotFoundException exception) {
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
+    }
+}
--- src/main/java/dev/mertkaanguzel/mediumclone/repository/UserRepository.java
+++ src/main/java/dev/mertkaanguzel/mediumclone/repository/UserRepository.java
@@ -4,6 +4,8 @@ package dev.mertkaanguzel.mediumclone.repository;
 import dev.mertkaanguzel.mediumclone.model.UserAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<UserAccount, Long> {
-    UserAccount getUserByUsername(String username);
+    Optional<UserAccount> getUserByUsername(String username);
 }
